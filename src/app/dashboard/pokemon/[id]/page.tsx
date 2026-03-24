b8da4fa9eeaa7613918307bc30f19e42cc769783
Camilo López <camiloj.lopez79@gmail.com>
2026-03-24 16:59:57 -0300

    Cargar información del Pokemon por ID

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,13 +1,27 @@
+import { Pokemon } from "@/src/pokemons";
+
 interface PokemonPageProps {
   params: { id: string };
 }
 
-export default async function PokemonPage(props: PokemonPageProps) {
-  const { id } = await props.params;
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache", // TODO: Cambiar a futuro
+  }).then((resp) => resp.json());
+
+  console.log("Se cargó: ", pokemon.name);
+
+  return pokemon;
+};
+
+export default async function PokemonPage({ params }: PokemonPageProps) {
+  const { id } = await params;
+  const pokemon = await getPokemon(id);
 
   return (
     <div>
       <h1>Pokemon: {id}</h1>
+      <div>{JSON.stringify(pokemon)}</div>
     </div>
   );
 }
